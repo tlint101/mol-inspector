ab749ed2f61ddd179860204488e673a21554ed75
Tony Lin <tonyelin@gmail.com>
2025-05-09 15:22:57 +0800

    add ability to load xgb model as .json or .model format

--- mol_inspector/utils.py
+++ mol_inspector/utils.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+from xgboost import XGBClassifier
 
 os.environ["KERAS_BACKEND"] = "torch"
 
@@ -10,13 +11,21 @@ def load_model(model_path):
     """load model"""
     global loaded
     try:
+        # keras
         loaded = keras.saving.load_model(filepath=model_path)
     except Exception:
         try:
+            # pickle
             with open(model_path, "rb") as f:
                 loaded = pickle.load(f)
-        except Exception as e:
-            print(e)
+        except Exception:
+            try:
+                # json or model
+                loaded = XGBClassifier()
+                loaded.load_model(model_path)
+            except Exception as e:
+                print(f"Failed to load model: {e}")
+                loaded = None
 
     return loaded
 
